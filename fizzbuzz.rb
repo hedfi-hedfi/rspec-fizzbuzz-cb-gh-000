791100a3a65e129b95c9ac0f03dc787b0db8520a
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-15 14:47:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(int)
+
+  if int % 3 == 0
+    "fizz"
+  end
+  if int % 5 == 0
+    "buzz"
+  end
+  if int % 3 == 0 && int % 5 == 0
+    "fizzbuzz"    
+  end
 
 end
